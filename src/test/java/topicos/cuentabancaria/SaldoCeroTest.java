82ff826249ed5820b0f788d396ff966484c77c2b
Ivan Sambrana <sambranaivan@gmail.com>
2017-11-16 14:32:44 -0300

    primer commit

---
+++ src/test/java/topicos/cuentabancaria/SaldoCeroTest.java
@@ -0,0 +1,27 @@
+package topicos.cuentabancaria;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SaldoCeroTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void saldoCero()
+	{
+		CuentaBancaria cb = new CuentaBancaria();
+		int saldo = cb.getSaldo();
+		assertEquals(0, saldo);
+	}
+
+}
